118ae7f77fdfe180ddb9a2a48f38a069d175b4ef
ripples <564311873@qq.com>
2024-10-29 15:16:27 +0800

    feat: remove usage of data dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-DATA_DIR=/data
 TEMPLATE_DIR=/template
 BUSYBOX=$TEMPLATE_DIR/usr/bin/busybox
 
@@ -17,22 +16,25 @@ function init_ssh() {
   fi
 }
 
-if [ ! "$($BUSYBOX ls -A $DATA_DIR/usr)" ]; then
+if [ ! "$($BUSYBOX ls -A /usr)" ]; then
   echo "Copying system files from template"
   $BUSYBOX ln -sf $TEMPLATE_DIR/usr/bin /usr/
   $BUSYBOX ln -sf $TEMPLATE_DIR/usr/lib /usr/
   $BUSYBOX ln -sf $TEMPLATE_DIR/usr/lib64 /usr/
-  rsync -al --delete --exclude=/root --exclude=/home $TEMPLATE_DIR/ $DATA_DIR/
+  rsync -al --delete $TEMPLATE_DIR/etc/ /etc/
+  rsync -al --delete $TEMPLATE_DIR/opt/ /opt/
+  rsync -al --delete $TEMPLATE_DIR/usr/ /usr/
+  rsync -al --delete $TEMPLATE_DIR/var/ /var/
 
-  if [ ! "$(ls -A $DATA_DIR/root)" ]; then
+  if [ ! "$(ls -A /root)" ]; then
     echo "Copying root directory from template"
-    rsync -al $TEMPLATE_DIR/root/ $DATA_DIR/root/
-    init_ssh root $DATA_DIR/root
+    rsync -al $TEMPLATE_DIR/root/ /root/
+    init_ssh root /root
   fi
 
-  if [ ! "$(ls -A $DATA_DIR/home)" ]; then
+  if [ ! "$(ls -A /home)" ]; then
     echo "Copying home directory from template"
-    rsync -al $TEMPLATE_DIR/home/ $DATA_DIR/home/
+    rsync -al $TEMPLATE_DIR/home/ /home/
   fi
 fi
 
